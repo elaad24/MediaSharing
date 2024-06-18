92ea07e01ccfafa787ccd5df061522ba1034896a
Elad Dadon <elad@swap-commerce.com>
2024-06-18 11:37:08 +0200

    build structure for the project

---
+++ MediaSharing_front/src/App.tsx
@@ -0,0 +1,13 @@
+import Login from "./pages/Login/Login";
+
+function App() {
+  return (
+    <>
+      <div className="container" style={{}}>
+        <Login />
+      </div>
+    </>
+  );
+}
+
+export default App;
