e7be11b0498830ba925a640f676d452b215e28d3
elaad24 <elaad2408@gmail.com>
2024-06-24 15:56:49 +0200

    server  fixed the cookie transfer problem

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,10 +6,12 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import cors from "cors";
+import { connectToDatabase } from "./config/db";
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
 import authHandler from "./routes/auth/authHandler";
+import spotifyHandler from "./routes/spotify/spotifyHandler";
 
 var app = express();
 
@@ -17,21 +19,25 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(logger("dev"));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/auth", authHandler);
-
 const corsOptions = {
-  origin: "http://localhost:4000",
+  origin: "http://localhost:5173",
   credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // Handle preflight requests
+app.use(express.json());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/auth", authHandler);
+app.use("/spotify", spotifyHandler);
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next: NextFunction) {
@@ -53,6 +59,7 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  connectToDatabase().catch(console.error);
 });
 
 export default app;
