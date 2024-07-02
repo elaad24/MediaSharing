2cd47576aeba4945de817971bbd0ace4039c59aa
elaad24 <elaad2408@gmail.com>
2024-07-02 16:50:29 +0200

    added functions and split the component

--- server/src/utils/spotify.ts
+++ server/src/utils/spotify.ts
@@ -1,6 +1,7 @@
-import axios from "axios";
+import axios, { AxiosHeaders, AxiosRequestHeaders } from "axios";
 import { SpotifyPlaylist } from "../interfaces/spotify";
-
+import { error } from "console";
+import globalVariable from "../general/globalVariable";
 export const getAccessToken = async (
   SPOTIFY_CLIENT_ID: string,
   SPOTIFY_CLIENT_SECRET: string
@@ -59,9 +60,81 @@ export const findVideoId = async (
   songArtist: string
 ): Promise<string> => {
   const data = await axios.get(
-    `https://www.youtube.com/results?search_query=${songName}+${songArtist}`
+    `${globalVariable.youtubeSearchVideoBaseLink}${songName}+${songArtist}`
   );
 
   const videoId = data.data.split("watch?v=")[1].slice(0, 11);
   return videoId;
+};
+
+export const getYoutubeFileDownloadLink = async (youtubeId: string) => {
+  try {
+    // this is 3 main steps to get the file data
+    // its an option that it will be 2 steps as well if the song is in the api cache memory
+    // if it in cache memory we skip step #3
+
+    // step #1 - get the sig from convertURL
+
+    const firstURL = `https://nu.ummn.nu/api/v1/init?p=y&23=1llum1n471&_=${Math.random()}`;
+    const Headers = {
+      Accept: " */*",
+      "Accept-Language": " en-US",
+      Connection: " keep-alive",
+      Origin: " https://ytmp3s.nu",
+      Referer: " https://ytmp3s.nu/",
+      "Sec-Fetch-Dest": " empty",
+      "Sec-Fetch-Mode": " cors",
+      "Sec-Fetch-Site": " cross-site",
+      "User-Agent":
+        " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+      "sec-ch-ua":
+        ' "Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+      "sec-ch-ua-mobile": " ?0",
+      "sec-ch-ua-platform": ' "macOS"',
+    };
+    const firstReq = await axios.get(firstURL, { headers: Headers });
+    if (firstReq.data.error !== "0") {
+      throw error("there is error in #1 step of getting file ", firstReq.data);
+    }
+
+    // #2 step
+
+    const convertURL = firstReq.data.convertURL;
+    // params
+    // v- is youtube link
+    // f - format of the file - mp3
+    // _ - Math.random
+
+    const youtubeLink = `${globalVariable.youtubeVideoBaseLink}${youtubeId}`;
+    const fileFormat = "mp3";
+    const randomNumber = () => Math.random();
+
+    const secondURL = `${convertURL}&v=${youtubeLink}&f=${fileFormat}&_=${randomNumber()}`;
+    const secondReq = await axios.get(secondURL, { headers: Headers });
+    if (secondReq.data.error !== 0) {
+      throw error("there is error in #2 step of getting file ", secondReq.data);
+    }
+
+    // if the song in the api cache memory we skip step #3
+
+    const secondDownloadURL = secondReq.data.downloadURL;
+    if (secondDownloadURL != undefined && secondDownloadURL != "") {
+      return secondDownloadURL;
+    }
+
+    const redirectURL = secondReq.data.redirectURL;
+
+    //#3 step
+    const thirdUrl = `${redirectURL}&v=${redirectURL}&f=${fileFormat}&_${randomNumber()}`;
+    const thirdReq = await axios.get(thirdUrl, { headers: Headers });
+    if (thirdReq.data.error !== 0) {
+      throw error("there is error in #3 step of getting file ", thirdReq.data);
+    }
+    const downloadURL = thirdReq.data.downloadURL;
+
+    // #4 return the downloadURL
+    return downloadURL;
+  } catch (error) {
+    console.error(error);
+  }
 };
